f1b815730242985a6f6f0804a62457b2b96325a5
unknown <omic.sanjin92@gmail.com>
2024-05-22 13:17:52 +0200

    Add third example of ReactJs

---
+++ Vjezba 3/src/components/SearchPolje.jsx
@@ -0,0 +1,8 @@
+const SearchPolje = ({ onFilterChange }) => {
+    return (
+      <input type="search" className="search-box" placeholder="pretraži" onChange={onFilterChange} />
+    );
+  };
+  
+  export default SearchPolje;
+  
